001084e097e451aa29c89b42a9bc8fed85c3b94f
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-01-03 13:19:23 +0530

    added dynamic logic inside site controller

--- model/siteModel.js
+++ model/siteModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose")
 
-const siteSchema = mongoose.Schema({
+
+const flatSchema = new mongoose.Schema({
+    flat_name: { type: String, default: null },
+    flat_image: { type: String, default: null },
+    flat_type: { type: String, default: null }
+});
+
+const buildingSchema = new mongoose.Schema({
+    block: { type: String, default: null },
+    flats: [flatSchema]
+});
+
+const siteSchema = new mongoose.Schema({
     site_name: { type: String, default: null },
-    site_image : { type: String, default: null },
+    site_image: { type: String, default: null },
     site_location: { type: String, default: null },
-    site_description : { type: String, default: null },
-    buildings: [
-        {
-            block: { type: String, default: null },
-            flats: [
-                {
-                    flat_name: { type: String, default: null },
-                    flat_image : [{ type: String, default: null }],
-                    flat_type: { type: String, default: null }
-                }
-            ]
-        },
-    ],
-}, { timestamps: true })
+    site_description: { type: String, default: null },
+    buildings: [buildingSchema]
+}, { timestamps: true });
+
+
 
 siteSchema.index({ site_name: 'text' });
 
--- model/userModelDashboard.js
+++ model/userModelDashboard.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 
+
+
 const userSchema = mongoose.Schema({
     username: { type: String, default: null },
     googleId: { type: String, default: null },
